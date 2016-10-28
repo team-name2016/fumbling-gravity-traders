4b509357c1064f293dd6d436209e15e3f17740ef
Clyde Grey <clyde.grey@gmail.com>
2016-10-28 10:59:44 -0400

    merged spotify js

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -190,24 +190,79 @@ allergyApp.init = function() {
 		$(this).val('');
 	});
 
-// $('.resultsLi').css('border','200px solid pink');
-// $('.resultsLi a').on('click',function(e){
-// 	e.preventDefault();
-// 	$(this).css('color','red');
-// 	alert('clyde');
+};
 
 
 
-// });
+// ****************
 
 
+var spotApp= {};
+
+spotApp.getSomething= function(){
+	var spotURL= "https://api.spotify.com/v1/search";
+	$('form').on('submit', function(e){
+		e.preventDefault();
+		var searchQuery = $('#saySomething').val();
+		$.ajax({
+			url: spotURL,
+			method: 'GET',
+			dataType: 'json',
+			data: {
+				type: 'playlist',
+				q: searchQuery
+			},
+	success: function(res){
+		var random = Math.floor(Math.random()*20);
+		var playlist = res.playlists.items[random];
+		var playlistID = playlist.uri;
+		$('.spotify').empty();
+		console.log (playlistID);
+		var iframe = '<iframe src="https://embed.spotify.com/?uri='+ playlistID + ' "width=300" height="380" frameborder="0" allowtransparency ="true" id="iframeID"></iframe>';
+		$('.spotify').append(iframe);
+		spotApp.displaySomething(res);
 
 
+		$('.spotify').on('click', function(){
+			var timer = +('1000');
+			console.log(timer);
+			var iframe = $("#iframeID");
+			console.log (iframe);
+			setTimeout(function() {
+			 iframe.remove();
+			}, timer);
+		});
+	}
+		});
+
+
+		});
+	};
+
+
+spotApp.displaySomething = function (){
 
 };
+
+spotApp.init= function(){
+	spotApp.getSomething();
+};
+
+
+// DOCUMENT READY ---------------------------------------
+
+
+
+
+// ****************
+
+
+
+
 
 // Doc ready, run init
 $(function() {
 	allergyApp.init();
+	spotApp.init();
 });
 
